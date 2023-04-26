4bf86266f6bb279d21784b11f908611922146aa8
Ammar shalati <ammar.shaa123@gmail.com>
2023-04-26 12:30:48 +0300

    Fixes

--- src/pages/car/Car.jsx
+++ src/pages/car/Car.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { PieChart, Pie, Tooltip, ResponsiveContainer } from 'recharts';
 import CarList from '../../components/carList/CarList';
 import AddIcon from '@mui/icons-material/Add';
 
 import "./car.css";
 
 const Car = () => {
+  const data = [
+    { name: 'Mangar', value: 500 },
+    { name: 'Electrical Car', value: 400 },
+    { name: 'Hybird Car', value: 300 },
+  ];
   return (
     <div className='car'>
       <div className="info">
@@ -29,6 +35,28 @@ const Car = () => {
       <div className="infoList">
         <div className="carList">
 					<CarList />
+        </div>
+        <div className="chartReservation">
+          <div className="title">
+            <p>free cars</p>
+          </div>
+          <div style={{ width: '100%', height: 300 }}>
+            <ResponsiveContainer width="100%" height="100%">
+              <PieChart width={400} height={400}>
+                <Pie
+                  dataKey="value"
+                  isAnimationActive={true}
+                  data={data}
+                  cx="50%"
+                  cy="50%"
+                  outerRadius={80}
+                  fill="#6b00c6"
+                  label
+                />
+                <Tooltip />
+              </PieChart>
+            </ResponsiveContainer>
+          </div>
         </div>
       </div>  
     </div>
--- src/pages/space/Space.jsx
+++ src/pages/space/Space.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { PieChart, Pie, Tooltip, ResponsiveContainer } from 'recharts';
 import SpaceList from '../../components/spaceList/SpaceList';
 import AddIcon from '@mui/icons-material/Add';
 
 import "./space.css";
 
 const Space = () => {
+  const data = [
+    { name: 'Mangar', value: 500 },
+    { name: 'emp with camp Car', value: 400 },
+    { name: 'emp with priv Car', value: 300 },
+  ];
   return (
     <div className='space'>
       <div className="info">
@@ -23,6 +29,28 @@ const Space = () => {
       <div className="infoList">
         <div className="spaceList">
 					<SpaceList />
+        </div>
+        <div className="chartReservation">
+          <div className="title">
+            <p>free Spaces</p>
+          </div>
+          <div style={{ width: '100%', height: 300 }}>
+            <ResponsiveContainer width="100%" height="100%">
+              <PieChart width={400} height={400}>
+                <Pie
+                  dataKey="value"
+                  isAnimationActive={true}
+                  data={data}
+                  cx="50%"
+                  cy="50%"
+                  outerRadius={80}
+                  fill="#6b00c6"
+                  label
+                />
+                <Tooltip />
+              </PieChart>
+            </ResponsiveContainer>
+          </div>
         </div>
       </div>  
     </div>
